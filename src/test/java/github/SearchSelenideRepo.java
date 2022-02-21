4ccf2be9313b26e50eacb154b52ef798848f18b4
tmolonushenko <tmolonushenko@gmail.com>
2022-02-21 15:00:41 +0300

    added test

---
+++ src/test/java/github/SearchSelenideRepo.java
@@ -0,0 +1,22 @@
+package github;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
+
+public class SearchSelenideRepo {
+    @Test
+    void shouldFindSelenideInGithub() {
+        //открыть страницу в github
+        open("https://github.com/");
+        //ввести в поле поиска selenide и нажать enter
+        $("[data-test-selector='nav-search-input']").setValue("selenide").pressEnter();
+        $$(".repo-list li").first().$("a").click();
+        // нажимаем на линк от первого результата поиска
+        //check : в заголовке встречаются selenide/selenide
+        $("h1").shouldHave(text("selenide / selenide"));
+
+    }
+}
---
+++ src/test/java/github/SearchSoftAssertions.java
@@ -0,0 +1,30 @@
+package github;
+
+import com.codeborne.selenide.Condition;
+import github.pages.PageObjectsSoftAssertions;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
+import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
+
+public class SearchSoftAssertions {
+    PageObjectsSoftAssertions pageObjectsSoftAssertions = new PageObjectsSoftAssertions();
+
+    String wikiFilter = "SoftAssertions";
+
+    @Test
+    void shouldSoftAssetionsInWikiGithub() {
+
+
+        open("https://github.com/selenide/selenide");
+
+        pageObjectsSoftAssertions.clickWiki();
+        pageObjectsSoftAssertions.checkSoftAssertions(wikiFilter);
+        pageObjectsSoftAssertions.goSoftAssertions();
+        pageObjectsSoftAssertions.checkJUnit5();
+    }
+}
